e7d6ebf254fba564e7f73b878f96ffa805b09a56
Sim4n6 <sim4n6@gmail.com>
2019-10-15 22:11:24 +0100

    IMP some vulturing

--- Stats-Bot.py
+++ Stats-Bot.py
@@ -2,7 +2,7 @@ import tweepy
 import os
 import logging
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -14,26 +14,12 @@ logger = logging.getLogger()
 
 def get_followers_details(api):
 
-    followers = []
-
     logger.info("Retrieving my followers ...")
-    followers = tweepy.Cursor(api.followers).items()
-    return followers
-
-
-def get_user_detail(api, i, follower):
-
-    logger.info("get user details needed ...")
-    user = api.get_user(follower)
-    logger.info(
-        f"Username {i}: {user.screen_name} - {user.name} - \n\t is following ? {user.following} - {user.id} - {user.description} - \n\tfollowers count:{user.followers_count} - Statuses count: {user.statuses_count}"
-    )
-
-    return user
+    return tweepy.Cursor(api.followers).items()
 
 
 @app.route("/")
-def hello_world():
+def index():
 
     logger.info("Started logging ...")
     api = create_api()
